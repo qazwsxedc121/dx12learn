e9e630c0bd955fed45a9d3aebc9a1ac304c4c117
qazwsxedc121 <qazwsxedc121@gmail.com>
2023-10-13 20:39:18 +0800

    init commit

---
+++ dx12learn/Main.cpp
@@ -0,0 +1,58 @@
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <iostream>
+#include "D3d12.h"
+
+using namespace std;
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+class D3D12App
+{
+	
+}
+
+ostream& XM_CALLCONV operator<<(ostream& os, FXMVECTOR v)
+{
+	XMFLOAT4 dest;
+	XMStoreFloat4(&dest, v);
+	os << "(" << dest.x << "," << dest.y << "," << dest.z << "," << dest.w << ")";
+	return os;
+}
+
+ostream& XM_CALLCONV operator<<(ostream& os, FXMMATRIX m)
+{
+	for (int i = 0; i < 4; ++i)
+	{
+		os << XMVectorGetX(m.r[i]) << "\t";
+		os << XMVectorGetY(m.r[i]) << "\t";
+		os << XMVectorGetZ(m.r[i]) << "\t";
+		os << XMVectorGetW(m.r[i]);
+		os << endl;
+	}
+	return os;
+}
+
+int main()
+{
+	XMMATRIX A(1.0f, 0.0f, 0.0f, 0.0f,
+			   0.0f, 2.0f, 0.0f, 0.0f,
+			   0.0f, 0.0f, 4.0f, 0.0f,
+			   1.0f, 2.0f, 3.0f, 1.0f);
+	XMMATRIX B = XMMatrixIdentity();
+	XMMATRIX C = A * B;
+	XMMATRIX D = XMMatrixTranspose(A);
+	XMVECTOR det = XMMatrixDeterminant(A);
+	XMMATRIX E = XMMatrixInverse(&det, A);
+	XMMATRIX F = A * E;
+
+	cout << "A = " << endl << A << endl;
+	cout << "B = " << endl << B << endl;
+	cout << "C = A * B = " << endl << C << endl;
+	cout << "D = transpose(A) = " << endl << D << endl;
+	cout << "det = determinant(A) = " << det << endl;
+	cout << "E = inverse(A) = " << endl << E << endl;
+	cout << "F = A * E = " << endl << F << endl;
+
+	return 0;
+}
